0597d2f835caf78a30c2f673bba125184e0cada1
Aryan Kumar <aryankumar1504@gmail.com>
2022-02-23 18:44:49 +0530

    Added font. Base styles

---
+++ src/components/PageTitle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from '../styles/modules/title.module.scss';
+
+function PageTitle({ children }) {
+	return <h1 className={style.title}>{children}</h1>;
+}
+
+export default PageTitle;
