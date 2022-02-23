643205505ab6b411354fc496244eec4c4e0e25f4
Aryan Kumar <aryankumar1504@gmail.com>
2022-02-23 22:42:09 +0530

    Style for todo item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
 import PageTitle from './components/PageTitle';
+
+import styles from './styles/modules/app.module.scss';
 
 function App() {
 	return (
 		<div className='container'>
 			<PageTitle>DOT TODO</PageTitle>
+			<div className={styles.app__wrapper}>
+				<AppHeader />
+				<AppContent />
+			</div>
 		</div>
 	);
 }
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
+import TodoModal from './TodoModal';
 
 function AppHeader() {
+	const [modalOpen, setModalOpen] = useState(false);
+
+	function toggleModal() {
+		setModalOpen((prev) => {
+			setModalOpen(!prev);
+		});
+	}
+
+	function closeModal() {
+		setModalOpen(false);
+	}
+
 	return (
 		<div className={styles.appHeader}>
-			<Button variant='primary' type='button'>
-				Click Me
-			</Button>
+			<div onClick={toggleModal}>
+				<Button variant='primary' type='button'>
+					Add Task
+				</Button>
+			</div>
 			<SelectButton>
 				<option value='all'>All</option>
 				<option value='incomplete'>Incomplete</option>
 				<option value='complete'>Complete</option>
 			</SelectButton>
+			{modalOpen && <TodoModal closeModal={closeModal} />}
 		</div>
 	);
 }
